ca6c94360f59c2568149c412807dae4783bd16b2
Amal Bose Mathew <57861549+amalbosemathew@users.noreply.github.com>
2021-08-04 18:07:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,56 @@
-import sys
-from tkinter import *
-import time
-
-def times():
-    current_time=time.strftime("%H:%M:%S")
-    clock.config(text=current_time)
-    clock.after(200,times)
+import tkinter as tk
+from time import strftime
 
 
-root  = Tk()
-root.geometry("500x250")
-clock=Label(root,font=("times",50,"bold"),bg="white")
-clock.grid(row=2,column=2,pady=25,padx=100)
-times()
+def light_theme():
+    frame = tk.Frame(root, bg="white")
+    frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+    lbl_1 = tk.Label(frame, font=('calibri', 40, 'bold'),
+                     background='White', foreground='black')
+    lbl_1.pack(anchor="s")
 
-digi = Label(root,text="Digital Clock",font="times 24 bold")
-digi.grid(row=0,column=2)
+    def time():
+        string = strftime('%I:%M:%S %p')
+        lbl_1.config(text=string)
+        lbl_1.after(1000, time)
+    time()
 
 
-nota=Label(root,text="hours  minutes   seconds   ", font="times 15 bold")
+def dark_theme():
+    frame = tk.Frame(root, bg="#22478a")
+    frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+    lbl_2 = tk.Label(frame, font=('calibri', 40, 'bold'),
+                     background='#22478a', foreground='black')
+    lbl_2.pack(anchor="s")
 
-nota.grid(row=3,column=2)
+    def time():
+        string = strftime('%I:%M:%S %p')
+        lbl_2.config(text=string)
+        lbl_2.after(1000, time)
+    time()
 
+
+root = tk.Tk()
+root.title("Digital-Clock")
+canvas = tk.Canvas(root, height=140, width=400)
+canvas.pack()
+
+frame = tk.Frame(root, bg='#22478a')
+frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+lbl = tk.Label(frame, font=('calibri', 40, 'bold'),
+                     background='#22478a', foreground='black')
+lbl.pack(anchor="s")
+
+def time():
+    string = strftime('%I:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
+time()
+
+menubar = tk.Menu(root)
+theme_menu = tk.Menu(menubar, tearoff=0)
+theme_menu.add_command(label="Light", command=light_theme)
+theme_menu.add_command(label="Dark", command=dark_theme)
+menubar.add_cascade(label="Theme", menu=theme_menu)
+root.config(menu=menubar)
 root.mainloop()
